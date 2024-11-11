e5de176b6dde4dde1fb933f85cacff2e52737037
Suryansh Sharma <suryansharma09@gmail.com>
2024-11-11 13:44:31 +0530

    using resourse fileds for serializatioin

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
-from flask_restful import Api, Resource , reqparse , abort
-from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Api, Resource , reqparse , abort , marshal_with , fields
+from flask_sqlalchemy import SQLAlchemy 
 
 # reqparser : helps send the needed data
 
@@ -27,7 +27,7 @@ with app.app_context():
 # names= {"tim":{"age":21 , "gen" :"male"}
 #         ,"jim":{"age":28 , "gen" :"male"}}
 
-class HelloWorld(Resource):     #inheriting from resource  : class as a reaosurce
+# class HelloWorld(Resource):     #inheriting from resource  : class as a reaosurce
 
     # def get(self):
     #     return{"message":"helllow"}
@@ -38,42 +38,62 @@ class HelloWorld(Resource):     #inheriting from resource  : class as a reaosurc
     # def get(self,name,test):
     #     return{"name":name, "test" :test}
     
-    def get(self,name):
-        return names[name]   
+    # def get(self,name):
+    #     return names[name]   
 
     
 # api.add_resource(HelloWorld,"/helloworld/<string:name>/<int:test>")      #registering it as a resource with endpoint helloworld
-api.add_resource(HelloWorld,"/helloworld/<string:name>")
+# api.add_resource(HelloWorld,"/helloworld/<string:name>")
 
 video_put_args= reqparse.RequestParser() #created reqparser obj
 
 #automatically parse thru the req thats being sent and makes sure it fits the guidelines defined 
 # and has the correct info in it
-videos= {}
+# videos= {}
 video_put_args.add_argument("name", type= str, help="Name odf video" , required= True)
 video_put_args.add_argument("views", type= int, help="views odf video",required= True)
 video_put_args.add_argument("likes", type= int, help="likes odf video",required= True)
 
 #for error or hint to sender  : help
 
-def if_video_id_doesnt_exist(video_id):
-    if video_id not in videos:
-        abort(404, message ="could not find the video")
+# def if_video_id_doesnt_exist(video_id):
+#     if video_id not in videos:
+#         abort(404, message ="could not find the video")
 
-def if_video_id_exists(video_id):
-    if video_id in videos:
-        abort(409, message ="video id exists already")
+# def if_video_id_exists(video_id):
+#     if video_id in videos:
+#         abort(409, message ="video id exists already")
+
+resource_field={            #deines hoe an object should be serialized
+    'id': fields.Integer,
+    'name': fields.String,
+    'views': fields.Integer,
+    'likes': fields.Integer,
+}
 
 class Video(Resource):
+    @marshal_with(resource_field)       #decorator serialises the obj(ex; result) using the fiels in resorce field
     def get(self, video_id):
-        if_video_id_doesnt_exist(video_id)      #to prevent crashing
-        return videos[video_id]
+        # if_video_id_doesnt_exist(video_id)      #to prevent crashing
+        result  = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(409, message='video id already taken')
+            abort(404, message='couldnt find video ')
+        return result
     
+    @marshal_with(resource_field) 
     def put(self, video_id):
-        if_video_id_exists(video_id)    
+        # if_video_id_exists(video_id)    
         args = video_put_args.parse_args()
-        videos[video_id] = args
-        return videos[video_id], 201
+        result= VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(409, message='video id already taken')
+        video= VideoModel(id=video_id, name=args['name'], views=args['views'], likes=args['likes'])
+        db.session.add(video)
+        db.session.commit()
+        
+        # videos[video_id] = args
+        return video, 201
     
     def delete(self, video_id):
         if_video_id_doesnt_exist(video_id)
